dea069c4e5a8805edab75c6553ae3add6719509b
David Pham <davidpham87gmail.com>
2019-08-02 08:42:36 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject finance-clash "0.1.0-SNAPSHOT"
+(defproject finance-clash "0.1.0"
   :description "finance-clash backend"
   :dependencies [[cider/cider-nrepl "0.21.0"]
                  [org.clojure/clojure "1.10.0"]
