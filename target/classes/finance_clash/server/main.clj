c6fd87efe613e2041db02c39aa83c646a3116008
David Pham <davidpham87@gmail.com>
2019-09-30 23:55:30 +0200

    Update

---
+++ target/classes/finance_clash/server/main.clj
@@ -0,0 +1,1 @@
+(do (clojure.core/ns finance-clash.server.main (:gen-class)) (clojure.core/defn -main [] ((do (clojure.core/require (quote ring.server.leiningen)) (clojure.core/resolve (quote ring.server.leiningen/serve))) (quote {:ring {:handler finance-clash.server/app, :open-browser? false, :stacktraces? false, :auto-reload? false, :auto-refresh? false}}))))
