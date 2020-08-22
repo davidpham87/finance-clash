8d313299e3b11c5c6fa0492521037f585d278979
David Pham <davidpham87@gmail.com>
2020-08-22 17:46:11 +0200

    feat: update

--- src/clj/finance_clash/db.clj
+++ src/clj/finance_clash/db.clj
@@ -1,225 +1,243 @@
 (ns finance-clash.db
-  (:require [next.jdbc :as jdbc]
-            [next.jdbc.result-set :as rs]
-            #_[datomic.api :as d]
-            [datahike.api :as d]
-            [datahike-postgres.core]
-            #_[crux.api :as crux]
-            #_[crux.jdbc]))
+  (:require [datomic.api :as d]))
 
-(def db
-  {:dbtype "sqlite"
-   :dbname "sql/finance-clash-database.db"})
+(def user-schema
+  "Defines a user"
+  [#:db{:ident       :user/name,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/one,
+        :doc         "User name"}
+   #:db{:ident       :user/id,
+        :valueType   :db.type/string,
+        :unique      :db.unique/identity,
+        :cardinality :db.cardinality/one,
+        :doc         "User id"}
+   #:db{:ident       :user/kind,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/one,
+        :doc         "Kind of user [:user.kind/professor, :user.kind/student]"}
+   #:db{:ident :user.kind/professor}
+   #:db{:ident :user.kind/student}])
 
-(def ds (jdbc/get-datasource db))
+(def lecture-schema
+  "A lecture is a collection of homeworks and students with a given defined
+  time window. Transactions are the answers by the students. It should be
+  mainly used for homework."
+  [#:db{:ident       :lecture/id,
+        :valueType   :db.type/string,
+        :unique      :db.unique/value,
+        :cardinality :db.cardinality/one,
+        :doc
+        "Identifier of the lecture. Usually each owner should have a unique ID."}
 
-(defn execute-query! [query]
-  (with-open [connection (jdbc/get-connection ds)]
-    (jdbc/execute! connection query
-                   {:builder-fn rs/as-unqualified-maps})))
+   #:db{:ident       :lecture/title,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/one,
+        :doc         "Name of the lecture"}
 
-#_(defn init! []
-    (jdbc/execute! ds ["
-create table user (
-  name varchar(32),
-  email varchar(255)
-)"])
+   #:db{:ident       :lecture/professors,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/many,
+        :doc         "Professor(s) of the lecture"}
 
-    (jdbc/execute! ds ["
-create table quiz_attempts (
-  user integer,
-  question integer,
-  chapter integer,
-  start varchar(32),
-  end varchar(32),
-)"])
+   #:db{:ident       :lecture/students,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/many,
+        :doc         "Students following the lecture."}
 
-    #_(jdbc/execute! ds ["
-drop table user
-"])
+   #:db{:ident       :lecture/start,
+        :valueType   :db.type/instant,
+        :cardinality :db.cardinality/one,
+        :doc         "Start date of the lecture"}
 
-    (jdbc/execute! ds ["
-insert into user(name, email)
-  values('David','david@davidolivier.pro')"])
+   #:db{:ident       :lecture/end,
+        :valueType   :db.type/instant,
+        :cardinality :db.cardinality/one,
+        :doc         "End date of the lecture"}
 
-    (jdbc/execute! ds ["
-insert into user(name, email)
-  values('vincent','vincent@davidolivier.pro')"])
+   #:db{:ident       :lecture/templates,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/many,
+        :doc         "Templates from which the lecture is taken"}
 
-    (jdbc/execute! ds ["
-SELECT rowid, * FROM user;
-"]))
+   #:db{:ident       :lecture/problems,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/many,
+        :doc         "Problem set offered to students for training or examination."}
+
+   #:db{:ident       :lecture/tags,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/many,
+        :doc         "Additional tags for the lecture for identifying them."}
+
+   #:db{:ident       :lecture.problems/title,
+        :valueType   :db.type/instant,
+        :cardinality :db.cardinality/one,
+        :doc         "Title of the set of problems."}
+
+   #:db{:ident       :lecture.problems/kind,
+        :valueType   :db.type/keyword,
+        :cardinality :db.cardinality/one,
+        :doc         "One of :exam, :training, :homework"}
+
+   #:db{:ident       :lecture.problems/tags,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/many,
+        :doc         "Tags of the problems."}
+
+   #:db{:ident       :lecture.problems/start,
+        :valueType   :db.type/instant,
+        :cardinality :db.cardinality/one,
+        :doc         "The beginning date of the problem set."}
+
+   #:db{:ident       :lecture.problems/deadline,
+        :valueType   :db.type/instant,
+        :cardinality :db.cardinality/one,
+        :doc         "Last time to return the problem set"}
+
+   #:db{:ident       :lecture.problems/questions,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/many,
+        :doc         "Questions of the problem set."}
+
+   #:db{:ident       :lecture.problems/shuffle?,
+        :valueType   :db.type/boolean,
+        :cardinality :db.cardinality/one,
+        :doc         "Whether to shuffle the questions in the problems."}
+
+   #:db{:ident       :lecture.problems/transactions,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/many,
+        :doc         "All the answers provided by the student."}
+
+   #:db{:ident       :lecture.problems.transaction/user,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/one,
+        :doc         "User who answered"}
+
+   #:db{:ident       :lecture.problems.transaction/question,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/one,
+        :doc         "Question which was answered."}
+
+   #:db{:ident       :lecture.problems.transaction/answer,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/one,
+        :doc         "Answer from the user."}
+
+   #:db{:ident       :lecture.problems.transaction/timestamp,
+        :valueType   :db.type/instant,
+        :cardinality :db.cardinality/one,
+        :doc         "When the answer has been submitted"}])
+
+
+(def quiz-schema
+  "Quizzes are collection of questions allowing to group questions for reuse."
+  [#:db{:ident       :quiz/title,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/one,
+        :doc         "Title of the quiz."}
+   #:db{:ident       :quiz/tags,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/many,
+        :doc         "Tags of the quiz (category, year)."}
+   #:db{:ident       :quiz/questions,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/many,
+        :doc         "Questions belonging to the quiz."}])
+
+(def question-answer-schema
+  "Questions and answers schema. A question can have be either a multiple choice
+  or a single string question. The answer can be either multiple or unique."
+  [#:db{:ident       :question/tags,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/many,
+        :doc         "Tags of the question (category, year)."}
+
+   #:db{:ident       :question/difficulty,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/one,
+        :doc         "Difficulty of the question."}
+   #:db{:ident :question.difficulty/easy}
+   #:db{:ident :question.difficulty/medium}
+   #:db{:ident :question.difficulty/hard}
+
+   #:db{:ident       :question/points,
+        :valueType   :db.type/keyword,
+        :cardinality :db.cardinality/one,
+        :doc         "Number of points (in order to override the default difficulty points)"}
+
+   #:db{:ident       :question/title,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/one,
+        :doc         "Title of the question."}
+
+   #:db{:ident       :question/question,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/one,
+        :doc         "Question that is asked to the student."}
+
+   #:db{:ident       :question/explanation,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/one,
+        :doc         "Markdown string to explain the answer."}
+
+   #:db{:ident       :question/answers,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/many,
+        :doc         "Possible answers to the student."}
+
+   #:db{:ident       :question/correct-answers,
+        :valueType   :db.type/ref,
+        :cardinality :db.cardinality/many,
+        :doc         "Possible answers to the student."}
+
+   #:db{:ident       :answer/description,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/many,
+        :doc         "Possible answers to the student."}
+
+   #:db{:ident       :answer/documentation,
+        :valueType   :db.type/string,
+        :cardinality :db.cardinality/many,
+        :doc         "Documentation to the question, for adding details."}])
+
+(def finanche-clash-schema
+  (into [] cat [user-schema lecture-schema quizz-schema question-answer-schema]))
+
 
 (def datomic-uri "datomic:free://localhost:4334/finance-clash")
 
-(d/create-database datomic-uri)
+(def cfg {:store {:backend :file
+                  :path    "sql/finance-clash-datahike"}
+          :schema-flexibility :write
+          :keep-history?      true})
 
-(def conn (d/connect datomic-uri))
-(def db (d/db conn))
+(def conn (atom (d/connect datomic-uri)))
+
+
+(defn get-db
+  ([] (get-db @conn))
+  ([conn] (d/db conn)))
 
 (set! *print-length* 250)
 
-@(d/transact conn [{:db/doc "Hello world"}])
-
-(def finanche-clash-schema
-  [{:db/ident :user/name
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "User name"}
-
-   {:db/ident :user/id
-    :db/valueType :db.type/string
-    :db/unique :db.unique/identity
-    :db/cardinality :db.cardinality/one
-    :db/doc "User id"}
-
-   {:db/ident :user/kind
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/one
-    :db/doc "Kind of user (professor, student)"}
-
-   {:db/ident :user/lectures
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Lectures the user to which the user is a member."}
-
-   {:db/ident :lecture/name
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/many
-    :db/doc "Name of the lecture"}
-
-   {:db/ident :lecture/start
-    :db/valueType :db.type/instant
-    :db/cardinality :db.cardinality/one
-    :db/doc "Start date of the lecture"}
-
-   {:db/ident :lecture/end
-    :db/valueType :db.type/instant
-    :db/cardinality :db.cardinality/one
-    :db/doc "End date of the lecture"}
-
-   {:db/ident :lecture/templates
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Templates from which the lecture is taken"}
-
-   {:db/ident :lecture/homework
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Questions belonging to the lecture."}
-
-   {:db/ident :lecture/exercises
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Exercises to train students."}
-
-   ;; exercises
-
-   {:db/ident :exercises/title
-    :db/valueType :db.type/instant
-    :db/cardinality :db.cardinality/one
-    :db/doc "Name of the exercises."}
-
-   {:db/ident :exercises/kind
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/one
-    :db/doc "One of :exam, :training, :homework"}
-
-   {:db/ident :exercises/tags
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/many
-    :db/doc "Tags of the exercises."}
-
-   {:db/ident :exercises/start
-    :db/valueType :db.type/instant
-    :db/cardinality :db.cardinality/one
-    :db/doc "The beginning date of the homework."}
-
-   {:db/ident :exercises/deadline
-    :db/valueType :db.type/instant
-    :db/cardinality :db.cardinality/one
-    :db/doc "Last time to return the homework"}
-
-   {:db/ident :exercises/quizz
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Quizz from which the exercises is taken from."}
-
-   {:db/ident :exercises/shuffle?
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/one
-    :db/doc "Whether to shuffle the questions in the exercises."}
-
-   ;; questions
-   {:db/ident :question/tags
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/many
-    :db/doc "Tags of the question (category, year)"}
-
-   {:db/ident :question/difficulty
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/one
-    :db/doc "Difficulty of the question. One of [:easy, :medium, :hard]"}
-
-   {:db/ident :question/points
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/one
-    :db/doc "Number of points (in order to override the default difficulty points)"}
-
-   {:db/ident :question/title
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Title of the question."}
-
-   {:db/ident :question/question
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Question that is asked to the student."}
-
-   {:db/ident :question/answers
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/many
-    :db/doc "Possible answers to the student."}
-
-   {:db/ident :question/correct-answers
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Possible answers to the student."}
-
-   {:db/ident :question/explanation
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Markdown string to explain the answer."}
-
-   ;; quizz
-
-   {:db/ident :quizz/tags
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/many
-    :db/doc "Tags of the quizz (category, year)"}
-
-   {:db/ident :quizz/title
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Title of the quizz."}
-
-   {:db/ident :quizz/questions
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Questions belonging to the quizz."}])
-
-(d/transact conn finanche-clash-schema)
-
-(->> (d/q '[:find ?e ?attr
-           :in $
-           :where
-           [?e :db/ident ?attr]]
-         (d/db conn))
-     (into [])
-     (sort-by second))
+(defn init []
+  (d/create-database datomic-uri)
+  (let [conn (d/connect datomic-uri)]
+    (d/transact conn finanche-clash-schema)
+    true))
 
 
 (comment
+
+  (->> (d/q '[:find ?e ?attr
+              :in $
+              :where
+              [?e :db/ident ?attr]]
+            (d/db @conn))
+       (into [])
+       (sort-by second))
 
   (def crux-db
     {:crux.node/topology '[crux.jdbc/topology]
@@ -252,53 +270,57 @@ SELECT rowid, * FROM user;
 )
 
 
-(comment
+;; (comment
 
-  (def cfg {:store              {:backend  :pg
-                                 :username "postgres"
-                                 :password "postgres"
-                                 :path     "/datahike"
-                                 :host     "localhost"
-                                 :port     5432}
-            :schema-flexibility :write
-            :keep-history?      true})
-
-  (d/create-database cfg)
-
-
-  (def conn (d/connect cfg))
-
-  (d/transact conn finanche-clash-schema)
-
-
-  ;; The first transaction will be the schema we are using:
-  (d/transact conn [{:db/ident :name
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one }
-                    {:db/ident :age
-                     :db/valueType :db.type/long
-                     :db/cardinality :db.cardinality/one }])
-
-  ;; Let's add some data and wait for the transaction
-  (d/transact conn [{:name  "Alice", :age   20 }
-                    {:name  "Bob", :age   30 }
-                    {:name  "Charlie", :age   40 }
-                    {:age 15 }])
-
-  (d/q '[:find ?e ?n ?a
-         :keys entity name age
-         :where
-         [?e :name ?n]
-         [?e :age ?a]]
-       @conn)
-
-  #_(d/transact conn [{:db/ident :name
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one }
-                    {:db/ident :age
-                     :db/valueType :db.type/long
-                     :db/cardinality :db.cardinality/one }])
+;;   (def cfg {:store              {:backend  :pg
+;;                                  :username "postgres"
+;;                                  :password "postgres"
+;;                                  :path     "/datahike"
+;;                                  :host     "localhost"
+;;                                  :port     5432}
+;;             :schema-flexibility :write
+;;             :keep-history?      true})
 
 
 
-  )
+
+
+;;   ;; The first transaction will be the schema we are using:
+;;   (d/transact conn [{:db/ident       :name
+;;                      :db/valueType   :db.type/string
+;;                      :db/cardinality :db.cardinality/one }
+;;                     {:db/ident       :age
+;;                      :db/valueType   :db.type/long
+;;                      :db/cardinality :db.cardinality/one }])
+
+;;   ;; Let's add some data and wait for the transaction
+;;   (d/transact conn [{:name "Alice", :age 20 }
+;;                     {:name "Bob", :age 30 }
+;;                     {:name "Charlie", :age 40 }
+;;                     {:age 15 }])
+
+;;   (d/q '[:find ?e ?n ?a
+;;          :keys entity name age
+;;          :where
+;;          [?e :name ?n]
+;;          [?e :age ?a]]
+;;        @conn)
+
+;;   (->> (d/q '[:find ?e ?attr
+;;               :in $
+;;               :where
+;;               [?e :db/ident ?attr]]
+;;             (d/db @conn))
+;;        (into [])
+;;        (sort-by second))
+
+;;   #_(d/transact conn [{:db/ident       :name
+;;                        :db/valueType   :db.type/string
+;;                        :db/cardinality :db.cardinality/one }
+;;                       {:db/ident       :age
+;;                        :db/valueType   :db.type/long
+;;                        :db/cardinality :db.cardinality/one }])
+
+
+
+;;   )
